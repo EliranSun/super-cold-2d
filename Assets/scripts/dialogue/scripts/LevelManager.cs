9757610ac75e734ee375f7fb87c43a958d289aba
Eliran Shemesh <112626543+eliransteps@users.noreply.github.com>
2023-10-12 17:17:39 +0300

    Update LevelManager.cs

--- Assets/scripts/dialogue/scripts/LevelManager.cs
+++ Assets/scripts/dialogue/scripts/LevelManager.cs
@@ -70,16 +70,14 @@ public class LevelManager : MonoBehaviour
 
     private void Awake()
     {
-        mainCamera.backgroundColor = Color.black;
-
-        // if (DateTime.Now.Hour is >= 6 and < 12)
-        //     mainCamera.backgroundColor = new Color(0.8f, 0.8f, 0.8f);
-        // else if (DateTime.Now.Hour is >= 12 and < 18)
-        //     mainCamera.backgroundColor = new Color(1, 1, 1);
-        // else if (DateTime.Now.Hour is >= 18 and < 22)
-        //     mainCamera.backgroundColor = new Color(0.5f, 0.5f, 0.5f);
-        // else if (DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 6)
-        //     mainCamera.backgroundColor = new Color(0.3f, 0.3f, 0.3f);
+        if (DateTime.Now.Hour is >= 6 and < 12)
+            mainCamera.backgroundColor = new Color(0.8f, 0.8f, 0.8f);
+        else if (DateTime.Now.Hour is >= 12 and < 18)
+            mainCamera.backgroundColor = new Color(1, 1, 1);
+        else if (DateTime.Now.Hour is >= 18 and < 22)
+            mainCamera.backgroundColor = new Color(0.5f, 0.5f, 0.5f);
+        else if (DateTime.Now.Hour >= 22 && DateTime.Now.Hour < 6)
+            mainCamera.backgroundColor = new Color(0.3f, 0.3f, 0.3f);
 
         _audioSource = GetComponent<AudioSource>();
     }
