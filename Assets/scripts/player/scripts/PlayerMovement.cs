6e3dd5b8da08351210be1adbfbc8d52c862b4480
Eliran Shemesh <piro27@gmail.com>
2023-12-13 09:28:25 +0200

    Ryan's animations improvements

--- Assets/scripts/player/scripts/PlayerMovement.cs
+++ Assets/scripts/player/scripts/PlayerMovement.cs
@@ -1,109 +1,149 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
-public class PlayerMovement : ObserverSubject
+namespace player.scripts
 {
-    [SerializeField] private bool isControllingTime;
-    [SerializeField] private GameObject target;
-    [SerializeField] private TimeController timeController;
-    [SerializeField] private bool isGodMode;
-    [SerializeField] private bool _isDead;
-    [SerializeField] private SpriteRenderer _spriteRenderer;
-    private CollectWeapon _collectWeapon;
-    private int _deathCount;
-    private bool _isWalking;
-    private Rigidbody2D _rigidbody;
-    private bool _triggeredCollectWeapon;
-
-    private void Start()
+    public class PlayerMovement : ObserverSubject
     {
-        _collectWeapon = GetComponent<CollectWeapon>();
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
+        private static readonly int IsWalking = Animator.StringToHash("isWalking");
+        private static readonly int IsVertical = Animator.StringToHash("isVertical");
+        private static readonly int IsTurningBack = Animator.StringToHash("isTurningBack");
+        [SerializeField] private bool isControllingTime;
+        [SerializeField] private GameObject target;
+        [SerializeField] private TimeController timeController;
+        [SerializeField] private bool isGodMode;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        [FormerlySerializedAs("_isDead")] [SerializeField]
+        private bool isDead;
 
-        if (_isDead)
-            return;
+        [FormerlySerializedAs("_spriteRenderer")] [SerializeField]
+        private SpriteRenderer spriteRenderer;
 
-        // TODO: Share with MoveRigidBodyParts.cs
-        var moveHorizontal = Input.GetAxis("Horizontal");
-        var moveVertical = Input.GetAxis("Vertical");
-        _isWalking = moveHorizontal != 0 || moveVertical != 0;
+        [SerializeField] private float speed = 5f;
+        private Animator _animator;
+        private bool _areObserversNotified;
+        private CollectWeapon _collectWeapon;
+        private int _deathCount;
+        private bool _isWalking;
+        private Rigidbody2D _rigidbody;
+        private bool _triggeredCollectWeapon;
 
-        ControlTime();
-        DetectNearWeapon();
-    }
-
-    private void OnDisable()
-    {
-        if (isControllingTime) ControlTime();
-    }
-
-    private void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Wall"))
+        private void Start()
         {
-            transform.position = transform.position;
-
-            if (col.gameObject.name == "GetLostBoundary")
-                GameObject.Find("LevelManager").GetComponent<LevelManager>().AfraidToGetLost();
-
-            if (col.gameObject.name == "CrossTheRoadBoundary")
-                GameObject.Find("LevelManager").GetComponent<LevelManager>().AfraidToCrossTheRoad();
-
-            if (col.gameObject.name == "NotHisHouseBoundary")
-                GameObject.Find("LevelManager").GetComponent<LevelManager>().NotHisHouse();
+            _collectWeapon = GetComponent<CollectWeapon>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
-        if (col.gameObject.CompareTag("Bullet") && !isGodMode)
-            DeclareDeath(col.collider);
-    }
-
-    private void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.CompareTag("Bullet") && !isGodMode)
-            DeclareDeath(col);
-    }
-
-    private void DeclareDeath(Collider2D collision)
-    {
-        _spriteRenderer.enabled = true;
-        _isDead = true;
-        var directionOfHit = transform.position - collision.transform.position;
-        var force = directionOfHit.normalized * 100f;
-        _rigidbody.AddForce(force);
-        timeController.isTimeSlowed = true;
-        GetComponent<MoveRigidBodyParts>().enabled = false;
-        NotifyObservers(PlayerActions.IsDead);
-    }
-
-    private void ControlTime()
-    {
-        if (_isWalking)
+        private void Update()
         {
-            if (isControllingTime && !timeController.isTimeSlowed)
-                timeController.SlowTime();
+            if (Input.GetKeyDown(KeyCode.R))
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            if (isDead)
+                return;
+
+            ControlTime();
+            DetectNearWeapon();
+            Move();
         }
-        else if (isControllingTime && timeController.isTimeSlowed)
+
+        private void OnDisable()
         {
-            timeController.NormalTime();
+            if (isControllingTime) ControlTime();
         }
-    }
 
-    private void DetectNearWeapon()
-    {
-        if (_triggeredCollectWeapon || !target) return;
-
-        var position1 = (Vector2)transform.position;
-        var distance = Vector2.Distance(target.transform.position, position1);
-        if (distance <= 6f && target)
+        private void OnCollisionEnter2D(Collision2D col)
         {
-            _collectWeapon.Trigger(target.transform);
-            _triggeredCollectWeapon = true;
+            if (col.gameObject.CompareTag("Wall"))
+            {
+                transform.position = transform.position;
+
+                if (col.gameObject.name == "GetLostBoundary")
+                    GameObject.Find("LevelManager").GetComponent<LevelManager>().AfraidToGetLost();
+
+                if (col.gameObject.name == "CrossTheRoadBoundary")
+                    GameObject.Find("LevelManager").GetComponent<LevelManager>().AfraidToCrossTheRoad();
+
+                if (col.gameObject.name == "NotHisHouseBoundary")
+                    GameObject.Find("LevelManager").GetComponent<LevelManager>().NotHisHouse();
+            }
+
+            if (col.gameObject.CompareTag("Bullet") && !isGodMode)
+                DeclareDeath(col.collider);
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.CompareTag("Bullet") && !isGodMode)
+                DeclareDeath(col);
+        }
+
+        private void Move()
+        {
+            var moveHorizontal = Input.GetAxis("Horizontal");
+            var moveVertical = Input.GetAxis("Vertical");
+            _isWalking = moveHorizontal != 0 || moveVertical != 0;
+
+            var flipX = spriteRenderer.flipX;
+            var isChangingDirectionToRight = moveHorizontal > 0 && flipX;
+            var isChangingDirectionToLeft = moveHorizontal < 0 && !flipX;
+            var isChangingDirectionToUp = moveVertical >= 0;
+
+            if (isChangingDirectionToRight || isChangingDirectionToLeft)
+                spriteRenderer.flipX = !spriteRenderer.flipX;
+
+            _animator.SetBool(IsWalking, _isWalking);
+            _animator.SetBool(IsVertical, moveVertical != 0);
+            _animator.SetBool(IsTurningBack, isChangingDirectionToUp);
+
+            var movement = new Vector2(moveHorizontal, moveVertical);
+            _rigidbody.velocity = movement * speed;
+
+            if (_isWalking && !_areObserversNotified)
+            {
+                NotifyObservers(DialogueTrigger.PlayerMoved);
+                _areObserversNotified = true;
+            }
+        }
+
+        private void DeclareDeath(Collider2D collision)
+        {
+            spriteRenderer.enabled = true;
+            isDead = true;
+            var directionOfHit = transform.position - collision.transform.position;
+            var force = directionOfHit.normalized * 100f;
+            _rigidbody.AddForce(force);
+            timeController.isTimeSlowed = true;
+            GetComponent<MoveRigidBodyParts>().enabled = false;
+            NotifyObservers(PlayerActions.IsDead);
+        }
+
+        private void ControlTime()
+        {
+            if (_isWalking)
+            {
+                if (isControllingTime && !timeController.isTimeSlowed)
+                    timeController.SlowTime();
+            }
+            else if (isControllingTime && timeController.isTimeSlowed)
+            {
+                timeController.NormalTime();
+            }
+        }
+
+        private void DetectNearWeapon()
+        {
+            if (_triggeredCollectWeapon || !target) return;
+
+            var position1 = (Vector2)transform.position;
+            var distance = Vector2.Distance(target.transform.position, position1);
+            if (distance <= 6f && target)
+            {
+                _collectWeapon.Trigger(target.transform);
+                _triggeredCollectWeapon = true;
+            }
         }
     }
 }
